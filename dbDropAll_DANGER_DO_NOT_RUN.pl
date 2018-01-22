c8537a5708f330622bddb270055533528e129d77
Nathalie Tate <nathalie.tate00@gmail.com>
2018-01-22 18:09:40 -0500

    drop all utility

---
+++ dbDropAll_DANGER_DO_NOT_RUN.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use DBI;
+
+my $dbUser = 'herculeze';
+my $dbPW='';
+my $database='herculezeTest';
+
+my $dbh = DBI->connect("dbi:mysql:","$dbUser","$dbPW");
+$dbh->do("use $database"); 
+
+$dbh->do("drop table Message;");
+$dbh->do("drop table Photo;");
+$dbh->do("drop table Job;");
+$dbh->do("drop table User;");
