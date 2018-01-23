2e29b3668ac442ef3eadc413ac3da1efee83a47c
Nathalie Tate <nathalie.tate00@gmail.com>
2018-01-22 20:59:14 -0500

    debug

--- dbMigrations.pl
+++ dbMigrations.pl
@@ -34,12 +34,12 @@ $dbh->do("create table Message(msgID varchar(32) primary key, toID varchar(32),
   fromID varchar(32), unread integer(1),subject varchar(64), messageTxt
   varchar(500), sent integer);");
 
-$dbh->do("create table AD(email varchar(320) primary key, password varchar(64),
-  token varchar(64), created integer, salt int);");
+$dbh->do("create table AD(email varchar(320), password varchar(64), token
+  varchar(64), created integer, salt int);");
 
 my $password = sha256_hex("admin12345");
 $dbh->do("insert into AD(email,password,salt) values('admin\@example.com',
-  $password, '12345');");
+  '$password', '12345');");
 
 $dbh->do("alter table Job add constraint fk_customerEmail foreign key
   (customerID) references User(userID);");
