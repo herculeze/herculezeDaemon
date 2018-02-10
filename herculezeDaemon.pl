35b47d0fd9b3aa7222101700c7141ae18b18b3f0
Nathalie Tate <nathalie.tate00@gmail.com>
2018-02-10 16:06:47 -0500

    database access

--- herculezeDaemon.pl
+++ herculezeDaemon.pl
@@ -11,3 +11,36 @@ use strict;
 #       - Update Job status
 #       - Inform Customer
 #       - Inform Driver
+
+
+use DBI;
+
+###############################################################################
+my $dbUser = "herculeze";                                                        
+my $dbPW = "";                                                                   
+my $database = "herculezeTest";  
+
+my $dbh = DBI->connect("dbi:mysql:","$dbUser","$dbPW");
+$dbh->do("use $database;");   
+###############################################################################
+
+my @jobIDs = selectAllUnconditional("Job","jobID");
+my $currentTime = time;
+
+sub selectAllUnconditional
+{
+	my $table = shift;
+	my $col = shift;
+
+	my $statement = "SELECT $col FROM $table;";
+	my $preparedStatement  = $dbh->prepare($statement);
+	$preparedStatement->execute();
+
+	my @tmp;
+
+	while (my @row = $preparedStatement->fetchrow_array())
+	{
+		push @tmp, $row[0];
+	}
+	return @tmp;
+}
