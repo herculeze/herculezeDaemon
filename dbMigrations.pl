9cbbb7382046643190cb10fc0035b238ea1e69a1
Nathalie Tate <nathalie.tate00@gmail.com>
2018-05-07 10:42:07 -0400

    Review table + new cols in User

--- dbMigrations.pl
+++ dbMigrations.pl
@@ -16,7 +16,7 @@ $dbh->do("use $database");
 $dbh->do("create table User(userID varchar(32) primary key, salt int, fName
   varchar(20), lName varchar(20), email varchar(320), password varchar(64),
   accType integer(1), authToken varchar(32), accountStatus integer(1) DEFAULT
-  '-1', verificationCode varchar(32));");
+  '-1', verificationCode varchar(32), bio varchar, profilePicID varchar(32));");
 
 $dbh->do("create table Job(jobID varchar(32) primary key, title varchar(50),
   weight integer, height integer, width integer, length integer, toAddr1
@@ -27,8 +27,8 @@ $dbh->do("create table Job(jobID varchar(32) primary key, title varchar(50),
   description varchar(500), winningBidID varchar(32), endTime integer, distance
   varchar(16), timeCreated integer, auctionType integer(1) );");
 
-$dbh->do("create table Photo(jobID varchar(32), fileName varchar(40), driverID
-  varchar(32));");
+$dbh->do("create table Photo(photoID varchar(32) primary key, jobID varchar(32),
+  fileName varchar(40), driverID varchar(32));");
 
 $dbh->do("create table Message(msgID varchar(32) primary key, toID varchar(32),
   fromID varchar(32), unread integer(1),subject varchar(64), messageTxt
@@ -40,9 +40,22 @@ $dbh->do("create table AD(email varchar(320), password varchar(64), token
 $dbh->do("create table Bid(bidID varchar(32) primary key, jobID varchar(32),
   driverID varchar(32), bidAmnt int);");
 
+$dbh->do("create table Review(reviewID varchar(32) primary key, reviewerID
+  varchar(32), revieweeID varchar(32), starRating int, reviewText varchar, 
+  date int);");
+
 my $password = sha256_hex("admin12345");
 $dbh->do("insert into AD(email,password,salt) values('admin\@example.com',
   '$password', '12345');");
+
+$dbh->do("alter table Review add constraint fk_customerID_Rev foreign key
+  (reviewerID) references User(userID);");
+
+$dbh->do("alter table Review add constraint fk_driverID_Rev foreign key
+  (revieweeID) references User(userID);");
+
+$dbh->do("alter table User add constraint fk_profilePic foreign key
+  (profilePicID) references Photo(photoID);");
 
 $dbh->do("alter table Job add constraint fk_customerID_Job foreign key
   (customerID) references User(userID);");
