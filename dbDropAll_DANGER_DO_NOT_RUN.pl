bd32b049f04b446006c1cc15f0037576f0192435
Nathalie Tate <nathalie.tate00@gmail.com>
2018-05-14 14:51:15 -0400

    fix drop order

--- dbDropAll_DANGER_DO_NOT_RUN.pl
+++ dbDropAll_DANGER_DO_NOT_RUN.pl
@@ -12,11 +12,12 @@ my $database = $ENV{"DATABASE"};
 my $dbh = DBI->connect("dbi:mysql:","$dbUser","$dbPW");
 $dbh->do("use $database"); 
 
-$dbh->do("drop table TruckPhoto;");
-$dbh->do("drop table AD;");
 $dbh->do("drop table Bid;");
+$dbh->do("drop table AD;");
+$dbh->do("drop table TruckPhoto;");
+$dbh->do("drop table JobPhoto;");
 $dbh->do("drop table Message;");
-$dbh->do("drop table Job;");
 $dbh->do("drop table Review;");
+$dbh->do("drop table Job;");
 $dbh->do("drop table User;");
 $dbh->do("drop table Photo;");
